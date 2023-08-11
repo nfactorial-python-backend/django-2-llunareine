b0e6c0c508447f94ef6197766a7f0a58bcfc60a5
Aiganym <llunareine@gmail.com>
2023-08-11 16:58:59 +0600

    feat: add comments

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 app_name = 'news'
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('<int:news_id>/', views.news_detail, name='news_detail')
-
+    path('<int:news_id>/', views.news_detail, name='news_detail'),
+    path('add/', views.add_news, name='add_news'),
 ]
