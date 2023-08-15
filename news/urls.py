2a43b81bbf8132944191b37abdd6094d48827c5b
Aiganym <llunareine@gmail.com>
2023-08-15 17:16:26 +0600

    feat: add forms and add update view

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import NewsUpdateView
 from . import views
 app_name = 'news'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('<int:news_id>/', views.news_detail, name='news_detail'),
     path('add/', views.add_news, name='add_news'),
+    path('<int:news_id>/edit/', NewsUpdateView.as_view(), name='news_update'),
 ]
--- news/views.py
+++ news/views.py
@@ -6,6 +6,11 @@ from .models import News, Comments
 from django.http import HttpResponse, HttpResponseRedirect
 
 
+from .forms import NewsForm
+
+from django.views import View
+
+
 def index(request):
     news_list = News.objects.order_by('-created_at')
     return render(request, 'news/index.html', {'news_list': news_list})
@@ -24,16 +29,33 @@ def news_detail(request, news_id):
     return render(request, 'news/detail.html', {'news': news,  'comments': comments})
 
 
+
 def add_news(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-
-        if title and content:
-            news = News(title=title, content=content, created_at=timezone.now())
-            news.save()
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save()
             return HttpResponseRedirect(reverse('news:news_detail', args=(news.pk,)))
+    else:
+        form = NewsForm()
+    return render(request, 'news/news_create.html', {'form': form})
 
-    return render(request, 'news/news_create.html')
+
+
+class NewsUpdateView(View):
+    template_name = 'news/news_update.html'
+
+    def get(self, request, news_id):
+        news = get_object_or_404(News, pk=news_id)
+        form = NewsForm(instance=news)
+        return render(request, self.template_name, {'form': form, 'news': news})
+
+    def post(self, request, news_id):
+        news = get_object_or_404(News, pk=news_id)
+        form = NewsForm(request.POST, instance=news)
+        if form.is_valid():
+            form.save()
+            return redirect('news:news_detail', news_id=news_id)
+        return render(request, self.template_name, {'form': form, 'news': news})
 
 
